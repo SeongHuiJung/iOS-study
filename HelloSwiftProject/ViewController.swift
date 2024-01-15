0773701ac4958446b20a781b6fe424db2403a572
Jseong <tjdgml150237@naver.com>
2024-01-15 13:38:36 +0900

    Initial Commit

---
+++ HelloSwiftProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloSwiftProject
+//
+//  Created by 정성희 on 2024/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
